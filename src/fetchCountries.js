572cdc2924f4400124beafbf24452d2e69d3bd14
AntonVlasenkoo <antonvlasenko2005@gmail.com>
2023-03-21 21:16:31 +0200

    додає перевірку інпуту

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,12 @@
+import Notiflix from 'notiflix';
 export default function fetchCountries(searchQuery) {
+  const queryParams = '?fields=name,capital,population,flags,languages';
   return fetch(
-    `https://restcountries.com/v3.1/name/${searchQuery}?fields=name,capital,population,flags,languages`
+    `https://restcountries.com/v3.1/name/${searchQuery}${queryParams}`
   ).then(response => {
+    if (!response.ok) {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+    }
     return response.json();
   });
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './css/styles.css';
 import fetchCountries from './fetchCountries.js';
 import debounce from 'lodash.debounce';
-
+import Notiflix from 'notiflix';
+//===========================================================
 const DEBOUNCE_DELAY = 300;
 const inputRefs = document.querySelector('input#search-box');
 const listRefs = document.querySelector('.country-list');
@@ -12,8 +13,7 @@ inputRefs.addEventListener('input', debounce(fetchInput, DEBOUNCE_DELAY));
 
 function fetchInput() {
   searchQuery = inputRefs.value.trim();
-  if (inputRefs.value !== '') {
-    console.log(searchQuery);
+  if (searchQuery !== '') {
     fetchCountries(searchQuery)
       .then(countre => MarkupGenerator(countre))
       .catch(error => console.log(error));
@@ -21,8 +21,20 @@ function fetchInput() {
 }
 
 function MarkupGenerator(country) {
-  listMarkupGenerator(country);
-  //   infoMarkupGenerator(country);
+  if (country.length > 10) {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+    //===========================================================
+  } else if (country.length >= 2 && country.length <= 10) {
+    infoRefs.innerHTML = '';
+    listMarkupGenerator(country);
+    //===========================================================
+  } else if (country.length === 1) {
+    listRefs.innerHTML = '';
+    infoMarkupGenerator(country);
+    //===========================================================
+  }
 }
 
 function infoMarkupGenerator(country) {
@@ -38,24 +50,19 @@ function infoMarkupGenerator(country) {
 
   const infoMarkup = `
 <h1><span><img src="${svg}" alt="" width="32" height="24"></span> ${official}</h1> 
-<p>Capital: ${capital}</p>
-<p>Population: ${population}</p>
-<p>languages: ${language}</p>
+<p class = "text">Capital: ${capital}</p>
+<p class = "text">Population: ${population}</p>
+<p class = "text">languages: ${language}</p>
  `;
   infoRefs.innerHTML = infoMarkup;
 }
 
 function listMarkupGenerator(country) {
-  console.log(country[0]);
-
   const listMatkup = country
     .map(({ name: { official }, flags: { svg } }) => {
-      console.log(official);
       return `<li class = "country-item"><span class = "centering"><img src="${svg}" alt="" width="32" height="24">  </span> <span class = "centering, text"> ${official}</span></li> `;
     })
-
     .join('');
 
-  console.log(listMatkup);
   listRefs.innerHTML = listMatkup;
 }
